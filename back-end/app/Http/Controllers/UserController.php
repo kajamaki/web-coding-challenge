a9b8755ec9730327c045734e49ba6f63c70c9134
mohamed elouadifi <mohammed.elouadifi@gmail.com>
2019-03-19 19:19:22 +0100

    Add sign in to back end

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
+use Cartalyst\Sentinel\Checkpoints\NotActivatedException;
+use Cartalyst\Sentinel\Checkpoints\ThrottlingException;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function login(LoginUserRequest $request)
+    {
+        try {
+            if (Sentinel::authenticate($request->all())) {
+                $user = Sentinel::check();
+                $token = $user->createToken('MyApp');
+                return response()->json(['status' => 'success', 'message' => 'logged in successfully', 'token' => $token->accessToken]);
+            } else
+                return response()->json(['status' => 'fail', 'message' => 'incorrect credentials'], 401);
+        } catch (ThrottlingException $e) {
+            $delay = $e->getDelay();
+            return response()->json(['status' => 'fail', 'message' => "you are banned for $delay secondes"], 401);
+        }
+    }
+
     public function register(RegisterUserRequest $request)
     {
         $user = Sentinel::registerAndActivate($request->all());
